180e190a3d8840de8becba7476be73a55ac493a8
E.V. Phillips <ercephillips@ufl.edu>
2020-08-20 13:25:51 -0400

    added anims on scroll, cursor effects

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 //Selections
 let controller;
 let slideScene;
+let pageScene;
+let mouse = document.querySelector('.cursor');
 
 //Event listeners
-
+window.addEventListener('mousemove', cursor);
+window.addEventListener("mouseover", activeCursor);
 //Functions
 
 function animateSlides(){
@@ -13,17 +16,68 @@ function animateSlides(){
     const sliders = document.querySelectorAll('.slide');
     const nav = document.querySelector('.nav-header');
     //loop over each slide
-    sliders.forEach(slide =>{
+    sliders.forEach((slide,index,slides) =>{
         const revealImg = slide.querySelector('.reveal-img');
         const img = slide.querySelector('img');
         const revealText = slide.querySelector('.reveal-text');
         //gsap event chain
         const slideTl = gsap.timeline({defaults: {duration: 1, ease: 'power2.inOut'}});
-        slideTl.fromTo(revealImg, {x: '5%'}, {x: '100%'});
+        slideTl.fromTo(revealImg, {x: '0%'}, {x: '100%'});
         slideTl.fromTo(img, {scale: 2}, {scale: 1}, '-=1');
         slideTl.fromTo(revealText, {x: '0%'}, {x: '100%'}, '-=0.75');
         slideTl.fromTo(nav, {y: '-100%'}, {y: '0%'}, '-=1');
+        //create scene
+        slideScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            triggerHook: 0.25,
+            reverse:false
+        })
+        // .addIndicators({
+        // colorStart: 'white',
+        // colorTrigger:'white',
+        // name: 'slide'})
+        .setTween(slideTl)
+        .addTo(controller);
+
+        //new animation
+        const pageTl = gsap.timeline();
+        let nextSlide = slides.length - 1 === index ? 'end' : slides[index+1];
+        pageTl.fromTo(nextSlide, {y: '0%'}, {y: '50%'});
+        pageTl.fromTo(slide, {opacity: 1, scale:1}, {opacity: 0, scale: 0.5});
+        pageTl.fromTo(nextSlide, {y: '50%'}, {y: '0%'}, '-=0.5');
+        //create scene
+        pageScene = new ScrollMagic.Scene({
+            triggerElement: slide,
+            duration: '100%',
+            triggerHook: 0
+        })
+        // .addIndicators({
+        //     colorStart: 'white',
+        //     colorTrigger:'white',
+        //     name: 'slide',
+        //     indent: 200})
+        .setPin(slide, {pushFollowers: false})
+        .setTween(pageTl)
+        .addTo(controller);
     });
 }
 
+function cursor(e){
+    mouse.style.top = e.pageY + 'px';
+    mouse.style.left = e.pageX + 'px';
+}
+
+function activeCursor(e){
+    const item = e.target;
+    if(item.id === 'logo' || item.classList.contains('burger') || item.classList.contains('explore')){
+        mouse.classList.add('hover-active');
+    }else{
+        mouse.classList.remove('hover-active');
+    }
+    if(item.classList.contains('explore')){
+        // gsap.to('.title-swipe', 1, {y = '0%'});
+    }else{
+        // gsap.to('.title-swipe', 1, {y = '0%'});
+    }
+}
 animateSlides();
